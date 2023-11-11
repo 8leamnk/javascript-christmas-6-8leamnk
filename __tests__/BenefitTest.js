ef2ec5560e85baa9c1793a6a1c59f8c1f543d7e3
8leamnk <silkviewrk@gmail.com>
2023-11-11 11:32:33 +0900

    test(BenefitTest): 혜택 내역과 총혜택 금액을 계산하는 기능을 테스트함
    테스트 내용:
    - 빠르게 피드백 받기 위해서 테스트, 추후에 테스트 코드 추가할 예정
    - 혜택 내역을 계산하는 기능을 테스트
    - 총혜택 금액을 계산하는 기능을 테스트

---
+++ __tests__/BenefitTest.js
@@ -0,0 +1,35 @@
+import Benefit from '../src/Benefit.js';
+
+describe('혜택 클래스 테스트', () => {
+  const DATE = 25;
+  const menu = new Map([
+    ['티본스테이크', 1],
+    ['초코케이크', 2],
+    ['레드와인', 1],
+  ]);
+  const TOTAL = 130000;
+  const GIFT = 25000;
+
+  test('혜택 금액이 올바르게 계산 된다.', () => {
+    const OUTPUT = 33446;
+
+    const benefitObj = new Benefit(DATE, menu, TOTAL, GIFT);
+    const benefit = benefitObj.getBenefit();
+
+    expect(benefit).toBe(OUTPUT);
+  });
+
+  test('혜택 내역이 올바르게 반환 된다.', () => {
+    const output = [
+      '크리스마스 디데이 할인: -3,400원',
+      '특별 할인: -1,000원',
+      '증정 이벤트: -25,000원',
+      '평일 할인: -4,046원',
+    ];
+
+    const benefitObj = new Benefit(DATE, menu, TOTAL, GIFT);
+    const benefit = benefitObj.getBenefitDetail();
+
+    expect(benefit).toEqual(output);
+  });
+});

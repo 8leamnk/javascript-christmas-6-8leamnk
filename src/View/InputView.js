5f7d8e153f556f49230a2d1b7759142b5254cb6b
8leamnk <silkviewrk@gmail.com>
2023-11-12 23:47:47 +0900

    refactor(InputView): 입력 기능 끝에 newline을 추가
    변경 내용:
    - newline을 상수화하여 이를 입력 메시지 끝에 추가
    - 기대하는 '12월 이벤트 플래너'의 예시 모습과 newline이 맞도록 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -3,13 +3,17 @@ import MESSAGE from '../constants/message.js';
 
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(MESSAGE.input.date);
+    const input = await Console.readLineAsync(
+      `${MESSAGE.input.date}${MESSAGE.newline}`,
+    );
 
     return input;
   },
 
   async readMenu() {
-    const input = await Console.readLineAsync(MESSAGE.input.menu);
+    const input = await Console.readLineAsync(
+      `${MESSAGE.input.menu}${MESSAGE.newline}`,
+    );
 
     return input;
   },

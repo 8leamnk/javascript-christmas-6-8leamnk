3e2003f5987b54b4830e42530d64c9be48ecbe06
8leamnk <silkviewrk@gmail.com>
2023-11-13 16:11:18 +0900

    test(DateTest): 사용자에게 입력받은 날짜를 받아오는 기능을 테스트함
    테스트 내용:
    - 사용자가 제대로 숫자값을 입력했을 때 날짜 정보가 제대로 반환되는지 테스트

--- __tests__/Model/DateTest.js
+++ __tests__/Model/DateTest.js
@@ -27,4 +27,34 @@ describe('사용자가 입력한 날짜 테스트', () => {
       }).toThrow('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
     },
   );
+
+  test.each([
+    ['3', { date: 3, monthAndDay: '12월 3일' }],
+    ['25', { date: 25, monthAndDay: '12월 25일' }],
+  ])(
+    '입력한 날짜가 정상적인 숫자 형태면 날짜 정보가 제대로 반환 된다.',
+    (input, output) => {
+      // when
+      const dateObject = new Date(input);
+      const dateInfo = dateObject.findOutDateInfo();
+
+      // then
+      expect(dateInfo).toEqual(output);
+    },
+  );
+
+  test.each([
+    ['10', { date: 4, monthAndDay: '12월 4일' }],
+    ['15', { date: 30, monthAndDay: '12월 29일' }],
+  ])(
+    '입력한 날짜가 정상적인 숫자 형태면 날짜 정보가 다른 숫자로 반환되지 않는다.',
+    (input, output) => {
+      // when
+      const dateObject = new Date(input);
+      const dateInfo = dateObject.findOutDateInfo();
+
+      // then
+      expect(dateInfo).not.toEqual(output);
+    },
+  );
 });

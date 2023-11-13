035acbe2600251e03b9415f37b0e15d8be628e7d
8leamnk <silkviewrk@gmail.com>
2023-11-13 16:42:07 +0900

    test(GiftTest): 증정 메뉴를 계산하는 기능을 추가로 테스트함
    변경 내용:
    - Gift의 경로가 변경되어 이를 반영함
    - 증정 메뉴가 제대로 표시 되는지에 대한 테스트 내용을 수정
    
    테스트 내용:
    - 총주문 금액에 따라 증정 이벤트가 제대로 발생하는지 테스트 케이스를 추가

--- __tests__/Model/GiftTest.js
+++ __tests__/Model/GiftTest.js
@@ -1,9 +1,11 @@
-import Gift from '../src/Gift.js';
+import Gift from '../../src/Model/Gift.js';
 
 describe('Gift 클래스를 테스트', () => {
   test.each([
     [120000, 25000, '샴페인 1개', '없음'],
+    [490000, 25000, '샴페인 1개', '없음'],
     [119000, 0, '없음', '샴페인 1개'],
+    [35000, 0, '없음', '샴페인 1개'],
   ])(
     '총주문 금액이 120,000원 이상이면 증정 이벤트가 적용된다.',
     (total, gift, correct, incorrect) => {
@@ -21,7 +23,7 @@ describe('Gift 클래스를 테스트', () => {
   test.each([
     [0, '없음'],
     [25000, '샴페인 1개'],
-  ])('증정 금액이 없음 또는 샴페인 1개로 표시 된다.', (input, output) => {
+  ])('증정 메뉴가 없음 또는 샴페인 1개로 표시 된다.', (input, output) => {
     expect(Gift.findOutGiftDetail(input)).toBe(output);
   });
 });

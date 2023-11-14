4d31f62a884a385cd415e9a5b8c488eee3ea76cf
8leamnk <silkviewrk@gmail.com>
2023-11-14 10:24:38 +0900

    refactor: 더 이상 사용하지 않는 코드를 삭제함
    삭제 내용:
    - 유틸 함수를 객체의 메서드로 변경함에 따라 더 이상 사용하지 않는 코드, 테스트를 삭제

--- src/utils/convertUtils.js
+++
@@ -1,9 +0,0 @@
-import VALUE from '../constants/value.js';
-
-const convertCurrencyUnit = (number) => {
-  const stringOfCurrencyUnit = number.toLocaleString('ko-KR');
-
-  return `${stringOfCurrencyUnit}${VALUE.unit.won}`;
-};
-
-export default convertCurrencyUnit;

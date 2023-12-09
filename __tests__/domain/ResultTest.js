538c5d639553417b07ce4b28e5e5584c2a862120
8leamnk <silkviewrk@gmail.com>
2023-12-09 20:14:35 +0900

    test: 12월 이벤트 배지 계산 기능 테스트

--- __tests__/domain/ResultTest.js
+++ __tests__/domain/ResultTest.js
@@ -13,4 +13,17 @@ describe('이벤트가 적용된 결과 테스트', () => {
     // then
     expect(payment).toBe(OUTPUT);
   });
+
+  test.each([
+    [-20000, '산타'],
+    [-10000, '트리'],
+    [-5000, '별'],
+    [0, ''],
+  ])('12월 이벤트 배지 계산 기능 테스트', (totalBenefit, output) => {
+    // when
+    const payment = Result.findOutBadge(totalBenefit);
+
+    // then
+    expect(payment).toBe(output);
+  });
 });

3146e81e2fcaab84a5fc2331c3ae12c672531037
8leamnk <silkviewrk@gmail.com>
2023-12-09 18:39:34 +0900

    feat: 12월 이벤트 배지 계산 기능 추가

--- src/domain/Result.js
+++ src/domain/Result.js
@@ -1,6 +1,24 @@
 const Result = {
+  BADGE: [
+    ['산타', 20000],
+    ['트리', 10000],
+    ['별', 5000],
+  ],
+
   calculatePayment(total, discount) {
     return total + discount;
+  },
+
+  findOutBadge(totalBenefit) {
+    for (let index = 0; index < this.BADGE.length; index += 1) {
+      const [badge, amount] = this.BADGE[index];
+
+      if (totalBenefit >= amount) {
+        return badge;
+      }
+    }
+
+    return '';
   },
 };
 
